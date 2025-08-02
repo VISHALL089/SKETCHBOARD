74b284ef73a6a077c3cb3b0cc6f6d07133ad5518
VISHALL089 <crazyrock0106@gmail.com>
2025-08-02 19:06:32 +0530

    second commet

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ let app = express();
 let httpServer = require("http").createServer(app);
 let io = require("socket.io")(httpServer);
 
+io.on("connect", (socket) => {
+  connections.push(socket);
+  console.log("$(socket.id) has connected");
+
+  socket.on("disconnect", (reason) => {
+    connection = connections.filter((con) => con.id !== socket.id);
+  });
+});
 app.use(express.static("public"));
 
 let PORT = process.env.PORT || 8088;
--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 let canvas = document.getElementById("canvas");
 
-canvas.width = window.innerWidth;
+canvas.width = 0.98 * window.innerWidth;
 canvas.height = window.innerHeight;
 
 let ctx = canvas.getContext("2d");
 
-ctx.moveTo(100, 200);
-ctx.lineTo(200, 200);
-ctx.stroke();
+let x;
+let y;
+let mouseDown = false;
+
+window.onmousedown = (e) => {
+  mouseDown = true;
+};
+window.onmouseup = (e) => {
+  mouseDown = false;
+};
+
+window.onmousemove = (e) => {
+  x = e.clientX;
+  y = e.clientY;
+
+  if (mouseDown) {
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
+};
